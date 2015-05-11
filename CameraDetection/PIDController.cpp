51e302b51665dfc9ea0663f2709e1839024f4c14
Peter Huang <peterh@blackmagicdesign.com>
2015-05-12 09:15:07 +1000

    remove PIDController files

--- CameraDetection/PIDController.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PIDController.cpp
-//  CameraDetection
-//
-//  Created by Peter Huang on 15/11/2014.
-//  Copyright (c) 2014 FlightDynamics. All rights reserved.
-//
-
-#include "PIDController.h"
